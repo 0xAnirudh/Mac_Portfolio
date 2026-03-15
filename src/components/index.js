ca52c4b1eb7981dcbb733c11a89cebacb3786386
0xAnirudh <anirudhchourey5@gmail.com>
2026-03-15 20:39:19 +0530

    add window-2 & 3: Articles/BlogPosts & Resume

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "#components/Navbar.jsx";
 import SplitText from "#components/SplitText.jsx";
 import Welcome from "#components/Welcome.jsx";
+import WindowControls from "#components/WindowControls.jsx";
 
-export { Navbar, SplitText, Welcome };
+export { Navbar, SplitText, Welcome, WindowControls };
--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -19,10 +19,10 @@ const WindowWrapper = (Component, windowKey) => {
 
         if (isOpen) {
           el.style.display = "block";
-          
+
           gsap.fromTo(
             el,
-            { scale: 0.8, opacity: 0, y: 40 },
+            { scale: 0.8, opacity: 0, y: 20 },
             { scale: 1, opacity: 1, y: 0, duration: 0.3, ease: "power3.out" }
           );
           return;
@@ -31,7 +31,7 @@ const WindowWrapper = (Component, windowKey) => {
         gsap.to(el, {
           scale: 0.9,
           opacity: 0,
-          y: 20,
+          y: 0,
           duration: 0.22,
           ease: "power2.in",
           onComplete: () => {
